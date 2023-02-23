28da55c7563dbcfaba393e7b1f4740fc81d99b5a
Caroline Lameirão <carolinelameirao@outlook.com>
2023-02-23 12:52:59 -0300

    feat: criação do projeto

---
+++ src/main/java/br/com/clcode/clcode/ClcodeApplication.java
@@ -0,0 +1,13 @@
+package br.com.clcode.clcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/clcode/clcode/ClcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clcode.clcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
